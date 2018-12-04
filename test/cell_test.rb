3ebe66fbf6a5f1ca8719d70ad15db1e87e4efb53
Julia Marco <marcootj@gmail.com>
2018-12-03 18:29:49 -0700

    Adds render tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -56,7 +56,7 @@ class CellTest < Minitest::Test
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
 
-    assert_equal false, cell.fired_upon?
+  assert_equal false, cell.fired_upon?
   end
 
   def test_cell_can_be_fired_upon
@@ -66,7 +66,7 @@ class CellTest < Minitest::Test
     cell.place_ship(cruiser)
     cell.fire_upon
 
-    assert_equal true, cell.fired_upon?
+  assert_equal true, cell.fired_upon?
   end
 
   def test_ship_is_hit_when_cell_fired_upon
@@ -76,8 +76,54 @@ class CellTest < Minitest::Test
     cell.place_ship(cruiser)
     cell.fire_upon
 
-    assert_equal 2, cell.ship.health
+  assert_equal 2, cell.ship.health
   end
+
+  def test_new_cell_render
+
+    cell_1 = Cell.new("B4")
+
+  assert_equal ".", cell_1.render
+  end
+
+  def test_cell_renders_m_if_missed
+skip
+    cell_1 = Cell.new("B4")
+    cell.fire_upon
+
+  assert_equal "M", cell_1.render
+  end
+
+  def test_cell_render_s_if_true
+skip
+    cell_2 = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+
+  assert_equal "S", cell_2.render(true)
+  end
+
+  def test_cell_renders_h_if_hit
+skip
+    cell_2 = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+
+  assert_equal "H", cell_2.render
+  end
+
+  def test_cell_renders_x_if_ship_sunk
+skip
+    cell_2 = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 1)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+
+  assert_equal "X", cell_2.render
+  end
+
+
 
 
 
