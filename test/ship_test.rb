9e0a025c12d855f639587f29b984e4e15c94b2b9
Julia Marco <marcootj@gmail.com>
2018-12-03 17:38:25 -0700

    Adds a _ in line 33

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,32 +8,38 @@ class ShipTest < Minitest::Test
   end
 
   def test_it_has_a_name
+
     cruiser = Ship.new('Cruiser', 3)
     assert_equal 'Cruiser', cruiser.name
   end
 
   def test_it_has_a_length
+
     cruiser = Ship.new('Cruiser', 3)
     assert_equal 3, cruiser.length
   end
 
   def test_it_has_health
+
     cruiser = Ship.new('Cruiser', 3)
     assert_equal 3, cruiser.health
   end
 
   def test_it_is_not_sunk
+
     cruiser = Ship.new('Cruiser', 3)
     assert_equal false, cruiser.sunk?
   end
 
   def test_it_can_be_hit
+
     cruiser = Ship.new('Cruiser', 3)
     cruiser.hit
-    assert equal 2, cruiser.health
+    assert_equal 2, cruiser.health
   end
 
   def test_it_can_sink
+
     cruiser = Ship.new('Cruiser', 3)
     cruiser.hit
     cruiser.hit
