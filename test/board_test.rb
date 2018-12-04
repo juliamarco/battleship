88813dd8dd0509ed90988a8f35034315c431ef93
Julia Marco <marcootj@gmail.com>
2018-12-04 10:57:19 -0700

    Creates first board tests

---
+++ test/board_test.rb
@@ -0,0 +1,29 @@
+require './lib/board'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib.cell'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+
+  board = Board.new
+
+  assert_instance_of Board, board
+  end
+
+  def test_valid_coordinates
+
+  board = Board.new
+
+  assert_equal true, board.valid_coordinate?("A1")
+  end
+
+  def test_invalid_coordinates
+
+  board = Board.new
+
+  assert_equal false, board.valid_coordinate?("E1")
+  end
+
+end
